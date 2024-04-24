70f97cebe0b54e538539f713f1031c28a64ad9bb
SADIK M SALEH <92797678+Surddheeq@users.noreply.github.com>
2024-04-24 19:50:35 +0100

    American Numbers

---
+++ cali_gen.py
@@ -0,0 +1,84 @@
+#Code Successfully written by SADIK MAGAJI SALEH , WEDNESDAY/2/2024, MADE IN KANO,NG.
+##============[KAYAN AIKI]==============##
+import sys,time,re,platform,struct,os,random,bs4,json,random,datetime,subprocess,platform,requests,base64,string,platform,mechanize,uuid
+from bs4 import BeautifulSoup as sop
+from urllib import request as req
+from concurrent.futures import ThreadPoolExecutor as transporter
+from zlib import decompress as dec
+from requests.exceptions import ConnectionError
+from pip._vendor import requests as requests
+
+
+def International_Num():
+	user = []
+	import random
+	import time
+	import os
+	os.system("cls\nclear")
+	import sys
+	print("            𝙵𝚁𝙴𝙴 INTERNATIONAL 𝙽𝚄𝙼𝙱𝙴𝚁𝚂 𝙱𝚈 𝚂𝙰𝙳𝙸𝙺 𝚁𝙸𝙳𝙴𝚁\n\n         This is an International number generator zone..\n")
+	type = input("\nNUMBERS STARTING POINT [Example: 209, 213, 310, 323, 408, 415, 424, 442, 510, 530, 559, 562, 619, 626, 650, 657, 661, 669, 707, 714, 747, 760, 805, 818, 831, 858, 909, 916, 925, 949, and 951. ETC ]: ")
+	limits = 5000000
+	print("DO YOU WANT TO SEE LIMITS??:\n[01]: Yes\n[02]: No")
+	Ans = input("Answer: ")
+	if Ans in ["Yes","01","1","YES"]:
+		print(f"The maximum limit to crack is '{limits}'")
+	else:
+		print("oh Great! ")
+		False
+	try:
+		limit = int(input("How many Numbers do you want to crack?: "))
+		if limit < 1:
+			print(f"Ouuch! cracking numbers should be above  {limit}")
+			return International_Num() 
+		elif limit > limits:
+			print(f"ooouch! cracking numbers should be less than what you provided which is  '{limit}'")
+			return International_Num() 
+	except ValueError:
+		time.sleep(3.5)
+		g = "="
+		print(f"'\nWhat you specified isn't a valid numeric number.. Please use valid Numbers to continue\n RESTARTING ENGINE..\n")
+		time.sleep(3.5)
+		linex = g *7
+		print(f"\n{linex}   M   A  I   N       M   E   N   U    {linex}")
+		time.sleep(5.0)
+		return International_Num() 
+	else:
+		a = 111
+		b = 999
+		secretA = 1111
+		secretB = 9999
+		try:
+		  	costom_pass = input("Custom pass:")
+		  	costom_pass = costom_pass.split(",")
+		  	with transporter(max_workers=30) as rider:
+		  		for total in range(limit):
+		  			tohtally = random.randint(a,b)
+		  			tutally = random.randint(secretA,secretB)
+		  			main_goal = f"+1({type}){tohtally}-{tutally}"
+		  			#print(totall_Numbers)
+		  			ids = main_goal
+		  			user.sort()
+		  			user.append(main_goal)#Dangerous!
+		  			Keep_resting_in_peace_daddy1 = main_goal[:-1]
+		  			Keep_resting_in_peace_daddy2 = main_goal[:-2]
+		  			Keep_resting_in_peace_daddy3 = main_goal[:-3]
+		  			Keep_resting_in_peace_daddy4 = main_goal[:-4:]
+		  			Keep_resting_in_peace_daddy5 = main_goal[:-5]
+		  			Keep_resting_in_peace_daddy6 = main_goal[-6:]
+		  			Keep_resting_in_peace_daddy7 = main_goal[-7:]
+		  			Keep_resting_in_peace_daddy8 = main_goal[-8:]
+		  			Keep_resting_in_peace_daddy9 = main_goal[-9:]
+		  			Keep_resting_in_peace_daddy11= main_goal[-10:]
+		  			Keep_resting_in_peace_daddy10 = main_goal[-11:]
+		  			passwordsword_list = [main_goal,Keep_resting_in_peace_daddy1,Keep_resting_in_peace_daddy2,Keep_resting_in_peace_daddy3,Keep_resting_in_peace_daddy4,Keep_resting_in_peace_daddy5,Keep_resting_in_peace_daddy6,Keep_resting_in_peace_daddy7,Keep_resting_in_peace_daddy8,Keep_resting_in_peace_daddy9,Keep_resting_in_peace_daddy10,Keep_resting_in_peace_daddy11]
+		  			passwordsword_list.extend(costom_pass)
+		  			print(ids,passwordsword_list)
+		  			#rider.submit(___random___,ids,passwordsword_list)
+		except Exception as e:exit(str(e))   			
+
+
+
+International_Num() 
+#SOURCE-CODE IS FREE, BUT PLEASE GIVE CREDIT
+                                        #  -surddheeq™        
